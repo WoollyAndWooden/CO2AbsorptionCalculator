bc8584098ac6a093a247f33f12c3c56e38e044ed
Konrad Głąb <73139334+Darnoker@users.noreply.github.com>
2022-11-29 14:54:55 +0100

    change methods and test (#2)

--- backend/co2-absorption-calculator/src/test/java/com/co2absorptioncalculator/co2absorptioncalculator/HealthTest.java
+++ backend/co2-absorption-calculator/src/test/java/com/co2absorptioncalculator/co2absorptioncalculator/HealthTest.java
@@ -1,33 +1,19 @@
 package com.co2absorptioncalculator.co2absorptioncalculator;
 
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-import org.junit.jupiter.api.AfterAll;
+import com.co2absorptioncalculator.co2absorptioncalculator.controller.HealthController;
+import com.co2absorptioncalculator.co2absorptioncalculator.service.HealthService;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-
-import java.io.IOException;
 
 public class HealthTest {
 
-    private static final CloseableHttpClient CLIENT = HttpClients.createDefault();
-    private static final String URL = "http://localhost:8080/health";
-
-    @AfterAll
-    public static void closeClient() throws IOException {
-        CLIENT.close();
-    }
-
     @Test
-    public void testHealthStatusCode() throws IOException {
-        CloseableHttpResponse response = CLIENT.execute(new HttpGet(URL));
-        String resultOfGetMethod = EntityUtils.toString(response.getEntity());
+    public void testHealthStatusCode()  {
+        HealthController healthController = new HealthController(new HealthService());
+        String expectedResult = "OK";
 
-        Assertions.assertEquals(HttpStatus.OK.value(), Integer.valueOf(resultOfGetMethod));
+        Assertions.assertEquals(expectedResult, healthController.getHealth());
     }
 }
 
