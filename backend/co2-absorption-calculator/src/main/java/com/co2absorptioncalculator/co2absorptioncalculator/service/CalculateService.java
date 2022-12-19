768cce621a709d469ed04240b8b0d0763cc63540
Darnoker <73139334+Darnoker@users.noreply.github.com>
2022-12-19 20:50:36 +0100

    add request to calculate

---
+++ backend/co2-absorption-calculator/src/main/java/com/co2absorptioncalculator/co2absorptioncalculator/service/CalculateService.java
@@ -0,0 +1,12 @@
+package com.co2absorptioncalculator.co2absorptioncalculator.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CalculateService {
+
+    // to modify
+    public Double calculate(){
+        return 123d;
+    }
+}
