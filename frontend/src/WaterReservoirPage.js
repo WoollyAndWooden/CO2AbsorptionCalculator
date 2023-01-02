f7110edb369c0b9fa167c7439e3cb456773061f4
Konrad Głąb <73139334+Darnoker@users.noreply.github.com>
2023-01-02 21:29:20 +0100

    CO2AC-19 fix masl page not showing (#13)

--- frontend/src/WaterReservoirPage.js
+++ frontend/src/WaterReservoirPage.js
@@ -57,7 +57,7 @@ export default function WaterReservoirPage() {
                         </label>
                     </div>
                     <Link to={{
-                        pathname: '/season',
+                        pathname: '/masl',
                         state: {
                             state: choiceList
                         }
