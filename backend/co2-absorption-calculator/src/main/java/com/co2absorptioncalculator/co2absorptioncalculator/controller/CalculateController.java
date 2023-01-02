10d4f01e8419decabcb09885b48e4665e5a10920
Konrad Głąb <73139334+Darnoker@users.noreply.github.com>
2023-01-02 23:04:01 +0100

    CO2AC-13 add calculation of co2 absorption (#15)

--- backend/co2-absorption-calculator/src/main/java/com/co2absorptioncalculator/co2absorptioncalculator/controller/CalculateController.java
+++ backend/co2-absorption-calculator/src/main/java/com/co2absorptioncalculator/co2absorptioncalculator/controller/CalculateController.java
@@ -3,6 +3,7 @@ package com.co2absorptioncalculator.co2absorptioncalculator.controller;
 import com.co2absorptioncalculator.co2absorptioncalculator.service.CalculateService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin
@@ -16,7 +17,12 @@ public class CalculateController {
     }
 
     @GetMapping("/calculate")
-    public Double calculate() {
-        return calculateService.calculate();
+    public Double calculate(@RequestParam String forest, @RequestParam Double area,
+                            @RequestParam String age, @RequestParam String habitat,
+                            @RequestParam Double degree, @RequestParam String soil,
+                            @RequestParam String reservoir, @RequestParam String land,
+                            @RequestParam String location) {
+
+        return calculateService.calculate(forest, area, age, habitat, degree, soil, reservoir, land, location);
     }
 }
--- backend/co2-absorption-calculator/src/main/java/com/co2absorptioncalculator/co2absorptioncalculator/service/CalculateService.java
+++ backend/co2-absorption-calculator/src/main/java/com/co2absorptioncalculator/co2absorptioncalculator/service/CalculateService.java
@@ -1,11 +1,27 @@
 package com.co2absorptioncalculator.co2absorptioncalculator.service;
 
+import com.co2absorptioncalculator.co2absorptioncalculator.calculator.FactorCalculator;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculateService {
+    private final static int DIVISOR = 10000;
 
-    public Double calculate() { // to change
-        return 123d;
+    public Double calculate(String forest, Double area, String age, String habitat,
+                            Double degree, String soil, String reservoir, String land, String location) {
+
+        if(forest.equals("mature")) {
+            Double k = degree;
+            Double z = k;
+            Double habitatValue = FactorCalculator.getHabitatValue(habitat);
+            Double x = FactorCalculator.getX(age);
+            Double a = FactorCalculator.getA(soil);
+            Double b = FactorCalculator.getB(land);
+            Double C = FactorCalculator.getC(location);
+
+            return (habitatValue * (k * x * z * a * b * C)) * area / DIVISOR;
+        }
+
+        return 0d;
     }
 }
